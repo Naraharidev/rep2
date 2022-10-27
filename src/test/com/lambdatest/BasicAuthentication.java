5e3931a6a0d0b29a3aa1c272038486de7c2b92f3
Naraharidev <naraharidevarapu@gmail.com>
2022-10-27 06:22:08 +0000

    newupdated

--- src/test/com/lambdatest/BasicAuthentication.java
+++ src/test/com/lambdatest/BasicAuthentication.java
@@ -82,8 +82,48 @@ public class BasicAuthentication {
         System.out.println(reason);
     }*/
 
+    String username = "naraharidevarapu";
+	String accesskey = "NUT4CPjAcd0dDmNpPlqceCBlJJIJzCaCHapsJ2AaFEsLghb949";
+	public static RemoteWebDriver driver = null;
+	public String gridURL = "@hub.lambdatest.com/wd/hub";
+	boolean status = false;
+
+	public void setUp(String browser, String version, String platform) throws Exception {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("browserName", browser);
+		capabilities.setCapability("version", version);
+		capabilities.setCapability("platform", platform); // If this cap isn't specified, it will just get the any
+															// available one
+		capabilities.setCapability("build", "Test Scenario 1");
+		capabilities.setCapability("name", "Simple Form Demo");
+		capabilities.setCapability("network", true); // To enable network logs
+		capabilities.setCapability("visual", true); // To enable step by step screenshot
+		capabilities.setCapability("video", true); // To enable video recording
+		capabilities.setCapability("console", true); // To capture console logs
+		
+		
+
+		try {
+			driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
+			driver.get("https://www.amazon.in/?&ext_vrnc=hi&tag=googhydrabk1-21&ref=pd_sl_nxqtetlae_e&adgrpid=60571832564&hvpone=&hvptwo=&hvadid=486387378181&hvpos=&hvnetw=g&hvrand=16986360475431660608&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=20453&hvtargid=kwd-296458789801&hydadcr=14452_2154371&gclid=Cj0KCQjwteOaBhDuARIsADBqRegDUeoiqc5kXoh1RV2nqWGtF8phs9N3FmXYim54C5-XJCwMyJMdcRkaAuhzEALw_wcB");
+		} catch (MalformedURLException e) {
+			System.out.println("Invalid grid URL");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+
+
+
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
         BasicAuthentication test = new BasicAuthentication();
        System.out.println("Welcome to github");
+       try{
+       test.setUp("browser", "version", "platform");
+       }
+       catch(Exception e){
+        System.out.print(e.getMessage);
+       }
     }
 }
